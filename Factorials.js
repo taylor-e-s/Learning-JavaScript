99c21d682fa192b901dd59f4c2848cbbfde6114a
taylor-e-s <202455@pennmanor.net>
2021-04-08 08:30:54 -0400

    Started Factorials

---
+++ Factorials.js
@@ -0,0 +1,14 @@
+/*/ get values
+
+var prompt = require('prompt');
+prompt.start();
+prompt.get(['Enter_the_number_to_calculate_the_factorial'], function (err, result) {
+let factorial = (result.Enter_the_number_to_calculate_the_factorial)
+})
+// calculate factorial
+*/
+let i = null;
+let result = 1;
+for(let i = 1; i===8; i+=1);
+    result*=i
+console.log(result)
