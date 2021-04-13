a97b38347d889f8560df11fed72b3aaeacb8e269
taylor-e-s <202455@pennmanor.net>
2021-04-13 07:46:06 -0400

    Completed Sort

--- Sort.js
+++ Sort.js
@@ -1,11 +1,13 @@
 //turn the file to an array
 var fs = require('fs');
-var text = fs.readFileSync("takinginput.txt", 'utf-8');
-let array = text.split('\n')
+var text = fs.readFileSync("list10k.txt", 'utf-8');
+let array = text.split(' ')
+
 //sort the array
 const sorted = array.sort((a, b) => {  
     return b - a
   })
+
 //write back to a file
 fs.writeFile('sorted.txt', sorted,(error)=>{'error'
 });
